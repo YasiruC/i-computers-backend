2a2c6e3bb4094e62a2918e407efbd90cf269b3ef
YasiruC <info.yasiruc@gmail.com>
2026-04-24 16:47:16 +0530

    removing unnecessary files✏️

--- routers/studentRouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getAllStudent, saveStudent } from '../controllers/studentControle.js';
-
-const studentRouter = express.Router();
-
-studentRouter.get("/",getAllStudent);
-studentRouter.post("/",saveStudent);
-
-export default studentRouter;
